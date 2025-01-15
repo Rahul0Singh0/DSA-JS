92f085a04627b7f14d4893211da2426cb09d5b00
Rahul Singh <thesincererahul@gmail.com>
2025-01-15 13:14:39 +0530

    combination sum

--- Recursion/14_combinationSum_I.js
+++ Recursion/14_combinationSum_I.js
@@ -1,34 +1,36 @@
 // Leetcode 39
 function combination(candidate, target) {
-    function dfs(arr, target, idx) {
+    let arr = [];
+    function dfs(t, idx) {
+        if(t === 0) {
+            console.log(arr);
+            arr = [];
+            return;
+        }
+        if(idx >= candidate.length || t < candidate[idx]) return;
+
+        dfs(t, idx+1);
+        arr.push(candidate[idx]);
+        dfs(t-candidate[idx], idx);
+        arr.pop();
+    }
+    dfs(target, 0);
+    function method1(target, idx) {
         if(target === 0) {
             console.log(arr);
+            arr = [];
             return;
         }
         if(target < 0) return;
-        if(candidate[idx] <= target) {
-            arr.push(candidate[idx]);
-            dfs(arr, target-candidate[idx], idx);
-            arr.pop();
-        }
-        dfs(arr, target, idx+1);
-    }
-    // dfs([], target, 0);
-    function method1(arr, target, idx) {
-        if(target === 0) {
-            console.log(arr);
-            return;
-        }
-        if(target < 0) return;
-        for(let i = idx; i < arr.length; i++) {
-            arr.push(arr[i]);
-            method1(arr, target-candidate[i], i);
+        for(let i = idx; i < candidate.length; i++) {
+            arr.push(candidate[i]);
+            method1(target-candidate[i], i);
             arr.pop();
         }
     }
-    method1([], target, 0);
+    // method1(target, 0);
 }
 
-let candidate = [1,2,3,4,5];
-let target = 6;
+let candidate = [2,3,6,7];
+let target = 7;
 combination(candidate, target);
