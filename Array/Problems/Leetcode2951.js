7bc356c1d9a26a230b7ae61dd0f7b7b51a5aacc9
Rahul Singh <thesincererahul@gmail.com>
2025-02-15 20:43:38 +0530

    Added leetcode 2951

---
+++ Array/Problems/Leetcode2951.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} mountain
+ * @return {number[]}
+ */
+var findPeaks = function(mountain) {
+    let ans = [];
+    for(let i = 1; i < mountain.length-1; i++) {
+        if(mountain[i] > mountain[i+1] && mountain[i] > mountain[i-1]) {
+            ans.push(i);
+        }
+    }
+    return ans;
+};
