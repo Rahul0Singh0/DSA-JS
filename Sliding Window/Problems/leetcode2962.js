37701143f52639ea237613943b69e7f3752a6cd9
Rahul Singh <thesincererahul@gmail.com>
2025-05-04 13:17:58 +0530

    added optimized solution with time complexity O(N)

--- Sliding Window/Problems/leetcode2962.js
+++ Sliding Window/Problems/leetcode2962.js
@@ -14,3 +14,23 @@ var countSubarrays = function(nums, k) {
 // Brute Force Approach, O(n^2) solution
 
 // TLE , Time Complexity: O(n^2), Space Complexity: O(1)
+
+// Optimized Approach, O(n) solution
+// using sliding window technique
+var countSubarrays1 = function(nums, k) {
+    const mx_ele = Math.max(...nums);
+    let res = 0;
+    let cnt = 0;
+    let s = 0;
+    for (let e = 0; e < nums.length; e++) {
+        if (nums[e] == mx_ele) cnt++;
+
+        while (cnt == k) {
+            if (nums[s] == mx_ele) cnt--;
+            s++; // count the number of subarrays (atleast k times max element)
+        }
+
+        res += s;
+    }
+    return res;
+};
